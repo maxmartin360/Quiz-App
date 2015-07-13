b5c74b01e68a1feee2fa69d365399f3aacb8aae8
Max Martin <maxmartin360@gmail.com>
2015-07-12 23:52:11 -0400

    initial commit

---
+++ JS/app.js
@@ -0,0 +1,46 @@
+var question1 {
+	this.img: <url>
+	correctAnswer = 2
+	userAnswer: 0
+	answer1: "Angry Pizza Man"
+	answer2: "mario"
+	answer3: "That Crazy Plumber"
+	answer4: "Luigi"
+	}
+
+var question2 = Object.create(question1); 
+	question2.img = 
+	question2.correctAnswer = answer1
+	question2.userAnswer = 0
+	question2.answer1 = "Link"
+	question2.answer2 = "Zelda"
+	question2.answer3 = "shiek"
+	question2.answer4 = "Cheapest Smash Bros Character Ever"
+	
+
+var question3 = Object.create(question1); 
+	question3.img = 
+	question3.correctAnswer = answer4
+	question3.userAnswer = 0
+	question3.answer1 = "Halo"
+	question3.answer2 = "Cortana"
+	question3.answer3 = "Samus"
+	question3.answer4 = "Master Chief"
+
+var question4 = Object.create(question1); 
+	question4.img = <a href="http://imgur.com/7BrnQ52"><img src="http://i.imgur.com/7BrnQ52.jpg" title="source: imgur.com" /></a>
+	question4.correctAnswer = answer3
+	question4.userAnswer = 0
+	question4.answer1 = "Leon"
+	question4.answer2 = "Wesker"
+	question4.answer3 = "CHRIIIIISSSS!"
+	question4.answer4 = "Jake"
+
+var question5 = Object.create(question1); 
+	question5.img = <a href="http://imgur.com/UG2ZtJr"><img src="http://i.imgur.com/UG2ZtJr.jpg" title="source: imgur.com" /></a>
+	question5.correctAnswer = answer2
+	question5.userAnswer = 0
+	question5.answer1 = "Spyro"
+	question5.answer2 = "Crash Bandicoot"
+	question5.answer3 = "Cortex"
+	question5.answer4 = "Wizzy the Wombat"
